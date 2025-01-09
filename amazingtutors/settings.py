7f898a8b11c57b8f96d43e413d6e322a5cb2a76a
JeffChen <jeffchen1118@gmail.com>
2025-01-09 17:09:10 +0000

    fix Deployment init part5

--- amazingtutors/settings.py
+++ amazingtutors/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRETE_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'amazingtutors-3f0b6cb3b188.herokuapp.com',
 ]
 
