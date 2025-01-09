60f0f3fcf85a28c007f0384ff9a42c6c5ee1d4bd
JeffChen <jeffchen1118@gmail.com>
2025-01-09 17:26:26 +0000

    Deployment init part7

--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePage(TemplateView):
+    """
+    Displays the home page.
+    """
+    
+    template_name = 'index.html'
