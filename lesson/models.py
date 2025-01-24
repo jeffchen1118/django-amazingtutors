57edbfdc3c819267f9267e5c04ddcc920def23a3
JeffChen <jeffchen1118@gmail.com>
2025-01-24 10:28:44 +0000

    create Question Answer models

--- lesson/models.py
+++ lesson/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.text import slugify
 
 STATUS = ((0, "Draft"), (1, "Published"))
+QUESTIONTYPES = ((0, "Test"), (1, "Discussion"))
 
 
 # Create your models here.
@@ -33,3 +34,41 @@ class Note(models.Model):
 
     def __str__(self):
         return self.body
+
+
+class Question(models.Model):
+    lesson = models.ForeignKey(
+        Lesson, on_delete=models.CASCADE, related_name="questions"
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="questions")
+    body = (
+        models.TextField()
+    )  # Field for storing the question or assignment task or practice content
+    presetanswer = models.TextField(
+        blank=True, null=True
+    )  # Field for storing the predetermined answer
+    questiontype = models.IntegerField(choices=QUESTIONTYPES, default=0)
+    due_date = models.DateTimeField(
+        blank=True, null=True, default=None
+    )  # Field for storing the due date
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:50]
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="answers")
+    body = models.TextField()  # Field for storing the answer content
+    is_correct = models.BooleanField(default=False)
+    likes = models.IntegerField(default=0)
+    liked_list = models.JSONField(default=list)  # Requires Django 3.1+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:50]
